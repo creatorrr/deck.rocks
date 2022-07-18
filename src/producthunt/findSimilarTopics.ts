184df970d63c1c26142fa7c923d9af84d299200d
Diwank Singh Tomer <diwank.singh@gmail.com>
2022-07-18 13:35:41 +0530

    refactor: Slight performance improvement
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- src/producthunt/findSimilarTopics.ts
+++ src/producthunt/findSimilarTopics.ts
@@ -6,20 +6,25 @@ import VectorMath from "@seregpie/vector-math";
 import embedding from "../openai/embedding";
 import getTopicsWithEmbeddings from "./getTopicsWithEmbeddings";
 
+const preloadedTopicEmbeddings = getTopicsWithEmbeddings();
+
 export const findSimilarTopics = async (query, top = 3) => {
   const queryEmbedding = await embedding(query);
-  const topicEmbeddings = await getTopicsWithEmbeddings();
+  const topicEmbeddings = await preloadedTopicEmbeddings;
 
   // Calculate cosine similarities between topic embeddings and the query embedding
-  const querySimilarities = topicEmbeddings
+  // Return `top` similar topic slugs
+  const similarTopics = _(topicEmbeddings)
     .map(({ slug, embedding }) => ({
       slug,
       similarity: VectorMath.CosineSimilarity(queryEmbedding, embedding),
     }))
-    .sort((a, b) => b.similarity - a.similarity);
+    .sortBy("similarity")
+    .takeRight(top)
+    .map("slug")
+    .value();
 
-  // Return `top` similar topic slugs
-  return _(querySimilarities).map("slug").take(top).value();
+  return similarTopics;
 };
 
 export default findSimilarTopics;
