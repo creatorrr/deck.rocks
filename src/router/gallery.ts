0d9e9c755b1e68b6debb1bc761f7489ecc880ae1
Diwank Singh Tomer <diwank.singh@gmail.com>
2022-07-30 20:20:32 +0530

    feat: Randomize gallery
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- src/router/gallery.ts
+++ src/router/gallery.ts
@@ -1,6 +1,7 @@
 // router/gallery.ts
 
 import * as Koa from "koa";
+import _ from "lodash";
 
 import cache from "../clients/cache";
 import { maxGalleryItems } from "../env";
@@ -9,7 +10,8 @@ import Gallery from "../views/Gallery";
 
 export default async (ctx: Koa.Context) => {
   const { prefill, error } = ctx.query;
-  const keys = (await keysFromCache("magic", maxGalleryItems)) as any;
+  const _1000keys = (await keysFromCache("magic", 1000)) as any;
+  const keys = _.sampleSize(_1000keys, maxGalleryItems);
   const ideas = await Promise.all(keys.map((key: string) => cache.get(key)));
 
   await ctx.render(Gallery, {
