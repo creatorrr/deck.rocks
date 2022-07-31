1f30bbff402ba6b29192541ae99c274f3044a422
Diwank Singh Tomer <diwank.singh@gmail.com>
2022-07-31 20:22:39 +0530

    fix: Make examples sample from a larger pool
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- src/router/home.ts
+++ src/router/home.ts
@@ -9,7 +9,11 @@ import { getRandomFromCache, keysFromCache } from "../utils/cache";
 export default async (ctx: Koa.Context) => {
   const { prefill, error } = ctx.query;
   const haveExisting: boolean = (await keysFromCache("magic", 1)).length > 0;
-  const examples = _.map(await getRandomFromCache("magic", 3), "idea");
+  const examples = _(await getRandomFromCache("magic", 100))
+    .sampleSize(3)
+    .map("idea")
+    .value();
+
   examples.length === 0 &&
     examples.push(
       "Picnic box for families that want to spend time out in nature"
