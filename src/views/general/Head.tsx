b566c03e6fff08777fc1e81fcbf103491d61c08d
Diwank Singh Tomer <diwank.singh@gmail.com>
2022-08-03 12:28:21 +0530

    feat: Add favicon.ico
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- src/views/general/Head.tsx
+++ src/views/general/Head.tsx
@@ -41,6 +41,12 @@ export default ({
 
     <link
       rel="icon"
+      type="text/plain"
+      href="data:;charset=utf-8,🤘"
+      sizes="any"
+    />
+    <link
+      rel="icon"
       href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤘</text></svg>"
     />
 
