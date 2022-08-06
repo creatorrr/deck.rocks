575499b9249cca4989374159fba25db5e4c5f544
Diwank Singh Tomer <diwank.singh@gmail.com>
2022-08-06 14:13:50 +0530

    feat: Retry openai requests
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- src/clients/openai.ts
+++ src/clients/openai.ts
@@ -1,5 +1,12 @@
 // clients/openai.ts
 
+import axios from "axios";
+import axiosRetry from "axios-retry";
+axiosRetry(axios, {
+  retries: 3,
+  retryCondition: (_error) => true, // Retry no matter what error
+});
+
 import { Configuration, OpenAIApi } from "openai";
 
 import { apiKey } from "../env";
