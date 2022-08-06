a622b366ba6769c50a0860964ebf2f7592469694
Diwank Singh Tomer <diwank.singh@gmail.com>
2022-08-06 20:17:43 +0530

    fix: Fix deck printing
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

--- src/router/pdf.ts
+++ src/router/pdf.ts
@@ -25,7 +25,7 @@ export default async (ctx: Koa.Context) => {
   const config: Record<string, any> = {
     emulateScreenMedia: false,
     scrollPage: true,
-    waitFor: 500,
+    waitFor: 3000,
     "pdf.printBackground": true,
     ignoreHttpsErrors: true,
     attachmentName: `deck-${job_id}-${hash}.pdf`,
@@ -43,8 +43,10 @@ export default async (ctx: Koa.Context) => {
   });
 
   if (!pdfResponse.ok) {
-    Sentry.captureException(new Error(await pdfResponse.text()));
-    return ctx.throw(500, "The pdf service was unable to fulfil your request.");
+    const error: string = `The pdf service was unable to render job#(${job_id})&hash#(${hash})`;
+
+    Sentry.captureException(new Error(error));
+    return ctx.throw(500, error);
   }
 
   pdfResponse.headers.forEach((value, header) => ctx.set(header, value));
